7f7b3938f67801ff409475c723c9dcbdee3ed3eb
Jean Mixco <jean.pacheco@galileo.edu>
2026-02-08 20:37:10 -0600

    Logica interna de la App finalizada con el AppModule modificado para que Koin aprenda a crear el repositorio como el viewmodel. Ya con esto el repository maneja los datos, ViewModel prepara los datos usando StateFlow e Init carga los datos de forma automatica al generarse :D

--- app/src/main/java/com/example/streamui/di/AppModule.kt
+++ app/src/main/java/com/example/streamui/di/AppModule.kt
@@ -1,11 +1,17 @@
 package com.example.streamui.di
 
 import com.example.streamui.data.repository.SongRepository
+import com.example.streamui.viewmodel.SongViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
     // Aqui le vamos a enseñar a la app a como crear el repositorio.
-    // "single" es como decir que crea una sola instancia para toda la app (sirve para el ahorro de memoria).
+    // "single" es como decir que crea una sola instancia para toda la app (sirve para el ahorro de memoria), Koin va a aprender a crear el repositorio.
     single { SongRepository() }
+
+    // Aqui Koin va a aprender a crear el viewmodel.
+    //get() dice: Busca el repositorio que definimos arriba y usalo aqui.
+    viewModel { SongViewModel(get()) }
 }
 
